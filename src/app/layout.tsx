e1d41744cb5974c17adc94d3e730d15de362eaff
Spencer Miskoviak <smiskoviak@gmail.com>
2023-07-03 21:26:14 -0600

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import classNames from "classnames";
 import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
